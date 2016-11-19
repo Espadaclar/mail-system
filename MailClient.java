ec1553fd62d7df5ff8034e9a510c50f61c23de65
Espadaclar <eneroborde8002@gmail.com>
2016-11-19 19:54:16 +0100

    0171 add mt, restAutomaticForHoliday y mailAutomaticDeVacaciones para responder a emails de forma automática.

--- MailClient.java
+++ MailClient.java
@@ -11,7 +11,7 @@ public class MailClient
     private MailServer server;
     // The user running this client.
     private String user;
-
+    private boolean deVacaciones;  //--------permite dar una respuesta automáticamente, restAutomaticForHoliday();
     /**
      * Create a mail client run by user and attached to the given server.
      */
@@ -19,23 +19,47 @@ public class MailClient
     {
         this.server = server;
         this.user = user;
+        deVacaciones = false;  //------utilizado en restAutomaticForHoliday() y en mailAutomaticDeVacaciones().
     }
 
     /**
-     * 0171 Se pide que implementes un método en la clase MailClient que permita saber desde un cliente de correo electrónico cuántos emails 
-     * tenemos en el servidor para nosotros y que dicha información se muestre por pantalla. Importante: los correos no deben ser descargados
+     * 0171_1) método  que permita saber desde un cliente de correo electrónico cuántos emails tenemos en el servidor para nosotros
+     *  dicha información se muestre por pantalla. Importante: los correos no deben ser descargados
      * del servidor.
      */
     public void numEmails(){
         System.out.println("Tienes " + server.howManyMailItems(user)+ " correos en el servidor.");
     }
-    
+
+    /**
+     * 0171_2) método que habilite o deshabilite la respuesta automática. En caso de estar habilitada, cada vez que 
+     * invocamos al método getNextMailItem se lleva a cabo la respuesta automática. De inicio, la respuesta automática está 
+     * desactivada. 
+     */
+    public void restAutomaticForHoliday(){//
+        deVacaciones = true;
+    }
+
+    /**
+     * 0171_2) método que permit configurar el mensaje y el asunto que se enviará cuando se
+     *lleve a cabo una respuesta automática.
+     */
+    private void mailAutomaticDeVacaciones(){
+       MailItem item = server.getNextMailItem(user);  // 1º recupero el mensaje recibido.
+        if(item != null && deVacaciones == true){
+            sendMailItem(item.getFrom(), item.getSubject(), "\n Estamos de vacaciones.  \n"
+                +item.getMessage()); 
+        }//2º lo devuelvo con la respuesta de mensaje  automático.
+    }
+
     /**
      * Return the next mail item (if any) for this user.
      */
     public MailItem getNextMailItem()
     {
+        mailAutomaticDeVacaciones();
         return server.getNextMailItem(user);
+
     }
 
     /**
